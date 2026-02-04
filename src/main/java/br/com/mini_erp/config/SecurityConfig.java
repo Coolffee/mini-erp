b51d517f085bc019554086dedad1cd33f8e94d27
Hugo Da Costa Gomes <thunderbite2011@gmail.com>
2026-02-04 19:08:20 -0300

    SecConfig

--- src/main/java/br/com/mini_erp/config/SecurityConfig.java
+++ src/main/java/br/com/mini_erp/config/SecurityConfig.java
@@ -4,21 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity // Boa prática adicionar esta anotação
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/usuarios/login").permitAll()
-                .anyRequest().authenticated()
-                .and().httpBasic(); // temporário
+        http
+                .csrf(csrf -> csrf.disable()) // Desabilita CSRF usando Lambda
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Libera todos os endpoints sem autenticação
+                );
+
         return http.build();
     }
 
